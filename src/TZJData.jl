b0bf62e5acb5f93894efead9ec082f94cb5f5996
Curtis Vogt <curtis.vogt@gmail.com>
2023-07-29 21:14:35 -0400

    Create TZJData artifact constants

--- src/TZJData.jl
+++ src/TZJData.jl
@@ -2,4 +2,13 @@ module TZJData
 
 using Artifacts
 
+const ARTIFACT_DIR = artifact"tzjdata"
+
+const TZDATA_VERSION = let
+    artifact_dict = Artifacts.parse_toml(joinpath(@__DIR__, "..", "Artifacts.toml"))
+    url = first(artifact_dict["tzjdata"]["download"])["url"]
+    m = match(r"tzdata(?<version>\d{2}\d{2}?[a-z])", url)
+    m !== nothing ? m[:version] : error("Unable to determine tzdata version")
+end
+
 end
