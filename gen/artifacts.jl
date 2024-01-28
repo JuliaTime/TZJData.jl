05d4cb2af08daa9e0218ba14d9b8bc21ff4b2338
Curtis Vogt <curtis.vogt@gmail.com>
2024-01-28 00:20:49 -0600

    Iterating

--- gen/artifacts.jl
+++ gen/artifacts.jl
@@ -2,12 +2,16 @@ using TOML: TOML
 
 const PKG_ROOT = joinpath(@__DIR__(), "..")
 
-function gh_artifact_key(artifact_toml=joinpath(PKG_ROOT, "Artifacts.toml"))
+function gh_artifact(artifact_toml=joinpath(PKG_ROOT, "Artifacts.toml"))
     toml = TOML.parsefile(artifact_toml)
     tarball_artifact = only(toml["tzjdata"]["download"])
     tarball_filename = basename(tarball_artifact["url"])
     tarball_sha256 = tarball_artifact["sha256"]
 
     key = "$(tarball_filename)-$(tarball_sha256)"
-    return key
+    return (;
+        tarball_filename,
+        tarball_sha256,
+        key,
+    )
 end
