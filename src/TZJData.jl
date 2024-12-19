747a33bd285270b3eee07b71bccee71db3d344e8
Curtis Vogt <curtis.vogt@gmail.com>
2024-12-19 14:43:49 -0600

    Refactor comments

--- src/TZJData.jl
+++ src/TZJData.jl
@@ -2,13 +2,14 @@ module TZJData
 
 using Artifacts
 
+# Avoid using a constant to define the artifact directory as this will hardcode the path
+# to the location used during pre-compilation which can be problematic if the Julia depot
+# relocated afterwards. One scenario where this can occur is when this package is used
+# within a system image.
 artifact_dir() = artifact"tzjdata"
+
+# Deprecation for TZJData.jl v1
 Base.@deprecate_binding ARTIFACT_DIR artifact_dir() false
-# Why not simply use `const ARTIFACT_DIR = artifact"tzjdata"`?
-# The problem is that `ARTIFACT_DIR` would be set to the artifact path at the time of precompilation.
-# If the Julia depot is afterwards relocated,
-# `ARTIFACT_DIR` will continue to point to the old no-longer-valid location.
-# This occurs for example if TimeZones.jl is included inside a system image.
 
 const TZDATA_VERSION = let
     artifact_dict = Artifacts.parse_toml(joinpath(@__DIR__, "..", "Artifacts.toml"))
@@ -18,9 +19,5 @@ const TZDATA_VERSION = let
 end
 
 precompile(artifact_dir, ())
-# `@deprecate_binding ARTIFACT_DIR artifact_dir()`
-# will have already precompiled `artifact_dir()`
-# but once the deprecated binding is removed
-# an explicit precompile statement will be necessary.
 
 end
